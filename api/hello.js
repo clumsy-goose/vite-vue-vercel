13b059a66603bef52204e79e9552b7c8109269e8
corbinlin <corbinlin@tencent.com>
2026-07-02 15:28:19 +0800

    1

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,7 @@
 export default {
   async fetch(request) {
     const response = await fetch('https://api.vercel.app/products');
+    
     const products = await response.json();
     return Response.json(products);
   },
